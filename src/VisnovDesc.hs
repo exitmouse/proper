58ab87889a3a15b4611c7ca62deb72f25a4cad13
Jesse Selover <jselover@mit.edu>
2013-09-07 02:48:45 -0400

    Add backgrounds, and multiple poses, to VisnovDesc.

--- src/VisnovDesc.hs
+++ src/VisnovDesc.hs
@@ -7,12 +7,14 @@ import qualified Data.Map as M
 import qualified Data.Time as T
 
 data World = World
-  { characterMap :: M.Map PersonID Person
-  , paragraphMap :: M.Map ParaID Paragraph
+  { worldCharacterMap :: M.Map PersonID Person
+  , worldBackgroundMap :: M.Map BackgroundID Background
+  , worldParagraphMap :: M.Map ParaID Paragraph
   , worldTime :: T.TimeOfDay
   }
 
 type PersonID = String
+type PoseID = String
 type ParaID = String
 type StatName = String
 type BoolID = String
@@ -24,18 +26,19 @@ type Paragraph = [Event]
 
 type Position = (Double, Double)
 
-data Event = 
-    AdvanceTime T.TimeOfDay
-  | AffectStat PersonID StatName Int
-  | ExitStage PersonID
-  | InvertGlobalBool BoolID
-  | Line PersonID String
-  | MenuAsk Menu
-  | PlaceUnit PersonID FilePath Position
-  | SetGlobalBool BoolID Bool
-  | SetGlobalNote NoteID String
+data Event = AdvanceTime T.TimeOfDay
+           | AffectStat PersonID StatName Int
+           | ExitStage PersonID
+           | InvertGlobalBool BoolID
+           | Line PersonID String
+           | MenuAsk Menu
+           | PlaceUnit PersonID FilePath Position
+           | SetGlobalBool BoolID Bool
+           | SetGlobalNote NoteID String
 
 data Person = Person
-  { defaultFrame :: FilePath
-  , statBlock :: M.Map StatName Int
+  { personFrames :: M.Map PoseID FilePath
+  , personStatBlock :: M.Map StatName Int
   }
+
+type Background = FilePath
