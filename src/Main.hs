7bfb847e846b27fc4cee1d06a836736afde72204
Jesse Selover <jselover@mit.edu>
2013-09-08 09:40:54 -0400

    Demonstrate api in Main.hs more fully.

--- src/Main.hs
+++ src/Main.hs
@@ -19,25 +19,66 @@ loadWorld = setupWorld charMap bgMap
     charMap :: M.Map CharacterID (M.Map PoseID FilePath)
     charMap = M.fromList [ ("duderton", M.singleton "blank_stare" "img/duderton.png")
                          , ("archibald", M.singleton "blank_stare" "img/archibald.png")
+                         , ("pemberton", M.fromList [("blank_stare", "img/pemberton.png"), ("b", "img/pemberton_b.png")])
                          , ("box", M.singleton "base" "img/box.png")
                          ]
     bgMap :: M.Map BackgroundID FilePath
-    bgMap = M.singleton "paris" "img/paris.png"
+    bgMap = M.fromList [ ("paris", "img/paris.png")
+                       , ("space_wedge", "img/space_wedge.png")
+                       ]
 
 game :: Visnov Int ()
 game = do
   liftIO $ playBackground "song.wav"
   setBackground "paris"
-  e <- getCharacter "archibald"
-  as e $ do
+  a <- getCharacter "archibald"
+  p <- getCharacter "pemberton"
+  as a $ do
     pose "blank_stare"
-    say "Text"
-    say "Next line"
-    say "Third line"
-  getChoice [ ("Agree with him", charAgree e)
-            , ("Disagree!!", charAgree e)
+    say "Now I will terrorize Paris!!"
+    say "No one can stop me!!"
+    say "Not even... Pemberton!!"
+  as p $ do
+    pose "blank_stare"
+    say "--Not so fast!!"
+    say "Yes, it is me, Pemberton."
+    say "Transform!!"
+    pose "b"
+    say "Hiyaaa!"
+    say "How do you like my new form?"
+  getChoice [ ("Yes, it's quite nice", pemberNice)
+            , ("You are evil too!!", pemberEvil)
             , ("Fly a plane", flyplane)
             ]
+
+pemberEvil :: Visnov Int ()
+pemberEvil = do
+  a <- getCharacter "archibald"
+  p <- getCharacter "pemberton"
+  as p $ do
+    say "Now I will terrorize the space wedge!!"
+    background "space_wedge"
+    say "No one can stop me!!"
+    say "Not even... Archibald!!"
+  as a $ do
+    pose "blank_stare"
+    say "--Not so fast!!"
+    say "Yes, it is me, Archibald."
+    say "Wait we've done this before."
+  end "You were too silly and you died. You were in space, even!! You can't breathe in space, that's ridiculous. Stop it."
+
+pemberNice :: Visnov Int ()
+pemberNice = do
+  a <- getCharacter "archibald"
+  p <- getCharacter "pemberton"
+  as p $ do
+    say "My marshmallowy form belies my true ability!"
+  as a $ do
+    pose "blank_stare"
+    say "--Not so fast!!"
+    say "You are way too fast!!"
+    say "Seriously slow down!!"
+  end "You were too fast and you died."
 
 charAgree :: Character -> Visnov Int ()
 charAgree c = do
@@ -48,7 +89,7 @@ charAgree c = do
 
 flyplane :: Visnov Int ()
 flyplane = do
-  end "You died."
+  end "You flew a plane into a wall and died."
 
 loop :: Visnov Int ()
 loop = getChoice [ ("Exit", flyplane)
